a06dd2a75d46b919d04810a07661e4ae3fd31042
Hassanehassan <hassanalhassanj401@gmail.com>
2023-07-10 22:11:00 +0300

    Navbar and Home

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,55 @@
-import React from 'react'
+import React, { useState } from "react";
+import classes from "./Navbar.module.css";
+import { NavLink } from "react-router-dom";
+import { links } from "../assets/data";
 
 const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+  console.log(showMenu);
   return (
-    <div>
-      Navbar
-    </div>
-  )
-}
+    <nav className={classes.nav}>
+      <div
+        className={
+          showMenu
+            ? `${classes["nav__menu"]} ${classes["show-menu"]}`
+            : classes["nav__menu"]
+        }
+      >
+        <ul className={classes["nav__list"]}>
+          {links.map(({ name, icon, path }, index) => {
+            return (
+              <li className={classes["nav__item"]} key={index}>
+                <NavLink
+                  to={path}
+                  className={({ isActive }) =>
+                    isActive
+                      ? `${classes["nav__link"]} ${classes["active-nav"]}`
+                      : classes["nav__link"]
+                  }
+                  onClick={() => setShowMenu(!showMenu)}
+                >
+                  <span className={classes["nav__icon"]}>{icon}</span>
+                  <h3 className={classes["nav__name"]}>{name}</h3>
+                </NavLink>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+      <div
+        className={
+          showMenu
+            ? `${classes["nav__toggle"]} ${classes["animate-toggle"]}`
+            : classes["nav__toggle"]
+        }
+        onClick={() => setShowMenu(!showMenu)}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,11 +1,37 @@
-import React from 'react'
+import React from "react";
+import classes from "./Home.module.css";
+import profile from "../../assets/hassane.jpg";
+import { Link } from "react-router-dom";
+import { FaArrowRight } from "react-icons/fa";
 
 const Home = () => {
   return (
-    <div>
-      Home
-    </div>
-  )
-}
+    <section className={`${classes.home} ${classes.section}`}>
+      <img src={profile} alt="" className={classes["home__img"]} />
+
+      <div className={classes["home__content"]}>
+        <div className={classes["home__data"]}>
+          <h1 className={classes["home__title"]}>
+            <span>I'm Hassane Hassane.</span> Web Developper
+          </h1>
+          <p className={classes["home__description"]}>
+            I'm a web developper, Front-end & Back-end and I am passionate about
+            building excellent software thate improves the lives of those
+            arround me.
+          </p>
+          <Link to="/about" className={classes.button}>
+            More About Me{" "}
+            <span className={classes["button__icon"]}>
+              <FaArrowRight />
+            </span>
+          </Link>
+        </div>
+      </div>
+      <div className={classes['color__block']}>
+
+      </div>
+    </section>
+  );
+};
 
 export default Home;
